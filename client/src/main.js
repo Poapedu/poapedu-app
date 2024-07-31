5fbbcfbddf9dce7d405d88c324e471515ec40330
Jackie <jackietanyen@gmail.com>
2024-08-01 03:08:33 +0800

    Update main.js to have redirect logic

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import { createClient } from "@supabase/supabase-js";
 
-loadFonts()
+loadFonts();
 
-const app = createApp(App)
-app.use(router)
-app.use(vuetify)
-app.use(store)
-app.mount('#app')
+const supabase = createClient(
+  process.env.VUE_APP_SUPABASE_URL,
+  process.env.VUE_APP_SUPABASE_ANON_KEY
+);
+
+router.beforeEach(async (to, from, next) => {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const isAuthenticated = !!session;
+
+  if (
+    to.matched.some((record) => record.meta.requiresAuth) &&
+    !isAuthenticated
+  ) {
+    next({ name: "SignIn" });
+  } else {
+    next();
+  }
+});
+
+const app = createApp(App);
+app.use(router);
+app.use(vuetify);
+app.use(store);
+app.mount("#app");
