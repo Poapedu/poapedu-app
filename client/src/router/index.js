24a0233bb702c765d6ed4abd9cdedf1bc77b892e
Jackie <jackietanyen@gmail.com>
2024-08-01 03:08:59 +0800

    Update index.js to have requiresAuth meta

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,35 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LandingPage from '@/views/LandingPage.vue'
-import UserDashboard from '@/views/UserDashboard.vue'
-import EditProfile from '@/views/EditProfile.vue'
-import SignInPage from '@/views/SignInPage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LandingPage from "@/views/LandingPage.vue";
+import UserDashboard from "@/views/UserDashboard.vue";
+import EditProfile from "@/views/EditProfile.vue";
+import SignInPage from "@/views/SignInPage.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: LandingPage
+    path: "/",
+    name: "Home",
+    component: LandingPage,
   },
   {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: UserDashboard
+    path: "/dashboard",
+    name: "Dashboard",
+    component: UserDashboard,
+    meta: { requiresAuth: true },
   },
   {
-    path: '/edit-profile',
-    name: 'EditProfile',
-    component: EditProfile
+    path: "/edit-profile",
+    name: "EditProfile",
+    component: EditProfile,
+    meta: { requiresAuth: true },
   },
   {
-    path: '/signin',
-    name: 'Sign In',
-    component: SignInPage
-  }
-]
+    path: "/signin",
+    name: "Sign In",
+    component: SignInPage,
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-  })
-  
-export default router
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
