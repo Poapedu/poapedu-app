3801fbcbf4ef1a0a9690ad3c4aecf575b836d537
Jackie <jackietanyen@gmail.com>
2024-08-19 00:58:35 +0800

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -3,34 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 import "./scss/main.scss"
 import supabasePlugin from './plugins/supabase'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts();
-
-const supabase = createClient(
-  process.env.VUE_APP_SUPABASE_URL,
-  process.env.VUE_APP_SUPABASE_ANON_KEY
-);
-
-router.beforeEach(async (to, from, next) => {
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  const isAuthenticated = !!session;
-
-  if (
-    to.matched.some((record) => record.meta.requiresAuth) &&
-    !isAuthenticated
-  ) {
-    next({ name: "SignIn" });
-  } else {
-    next();
-  }
-});
 
 const app = createApp(App);
 app.use(router);
