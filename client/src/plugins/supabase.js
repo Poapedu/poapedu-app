5925ba34866b0275d308205b3f02fa819d87f5f1
Jackie <jackietanyen@gmail.com>
2024-08-19 00:58:31 +0800

    Update supabase.js

--- client/src/plugins/supabase.js
+++ client/src/plugins/supabase.js
@@ -1,31 +1,34 @@
-// src/plugins/supabase.js
-import { supabase } from '@/supabase'
-import { authStore } from '@/store/authStore'
-import router from '../router'
+import { supabase } from "@/supabase";
+import { authStore } from "@/store/authStore";
+import router from "../router";
 
 // Initialize the session
 supabase.auth.getSession().then(({ data: { session } }) => {
-  authStore.setSession(session)
-})
+  authStore.setSession(session);
+});
 
 // Listen for auth changes
 supabase.auth.onAuthStateChange((event, session) => {
-  if (event === 'SIGNED_OUT' || event === 'USER_DELETED') {
-    // delete any user data stored in localStorage
-    localStorage.removeItem('user')
-    // redirect to login page
-    router.push('/signin')
-  } else if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
-    // you can store any needed user data in localStorage here
-    const userData = session.user
-    localStorage.setItem('user', JSON.stringify(userData))
-    // optionally redirect to dashboard or home page
-    router.push('/dashboard')
+  const currentSession = authStore.session; // Access the session directly
+
+  if (
+    (event === "SIGNED_IN" || event === "TOKEN_REFRESHED") &&
+    currentSession?.user?.id !== session?.user?.id
+  ) {
+    authStore.setSession(session);
+    const userData = session.user;
+    localStorage.setItem("user", JSON.stringify(userData));
+    router.push("/dashboard");
+  } else if (event === "SIGNED_OUT" || event === "USER_DELETED") {
+    authStore.clearSession();
+    localStorage.removeItem("user");
+    router.push("/signin");
   }
-})
+});
+
 export default {
   install: (app) => {
-    app.config.globalProperties.$supabase = supabase
-    app.provide('supabase', supabase)
-  }
-}
+    app.config.globalProperties.$supabase = supabase;
+    app.provide("supabase", supabase);
+  },
+};
