c11c50d5e7567975be1911cc8e05a0075676ec1f
Jackie <jackietanyen@gmail.com>
2024-08-27 01:00:28 +0800

    Create render-build.sh

---
+++ server/render-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit
+
+npm install
+npx puppeteer install
+
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+  echo "...Copying Puppeteer Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else 
+  echo "...Storing Puppeteer Cache in Build Cache" 
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+fi
